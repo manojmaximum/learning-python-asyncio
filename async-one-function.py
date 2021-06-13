6bf208ca083314d1047ff1f11e0511608262f9bd
Manoj Kumar Ragupathi <ra.manojkumar@outlook.com>
2021-06-13 17:23:23 +0530

    async with one function - successfully running - added comments

--- async-one-function.py
+++ async-one-function.py
@@ -3,6 +3,9 @@ import time
 
 
 async def fetch_data():
+    """
+    Mimicks the data fetch delay and returns a hard typed data
+    """
     print('start fetching')
     data = '{"data":1}'
     await asyncio.sleep(4)
@@ -10,6 +13,9 @@ async def fetch_data():
     return data
 
 async def main():
+    """
+    Async main function
+    """
     start_time = time.time()
     task=asyncio.create_task(fetch_data())
     value = await task
