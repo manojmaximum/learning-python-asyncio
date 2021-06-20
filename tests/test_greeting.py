95a0af8e05ecd64f9ca8823d4eb2c72561fff680
Manoj Kumar Ragupathi <manojkumar.ragupathi@outlook.com>
2021-06-20 21:47:46 +0530

    2 tests

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -1,8 +1,15 @@
 import pytest
-from lib.greeting import greeting
+from lib.greeting import Greeting
 
 class Test_Greeting:
     
     def test_greet(self):
-        greet = greeting('Manoj')
-        assert greet == 'Hello manoj'
+        greet = Greeting()
+        full_greet=greet.greeting('Manoj')
+        assert full_greet == 'Hello Manoj'
+    
+    def test_invalid_string_for_name_in_greet_(self):
+        with pytest.raises(ValueError) as exp:
+            greet=Greeting()
+            greet.greeting(2)
+        assert str(exp.value)== 'Invalid name'
